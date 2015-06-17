1efb1ccaea93aea0d6d10ab9a639561cf5dbd869
Renan <Renan@Renan-PC>
2015-06-17 19:33:11 -0300

    Improved  the Documentation

--- src/com/renangomes/alexarankcache/AlexaRank.java
+++ src/com/renangomes/alexarankcache/AlexaRank.java
@@ -1,3 +1,12 @@
+/**
+ * ALEXA RANK CACHE SYSTEM
+ *
+ * We use the Alexa Rank Top 1M file in order to build a cache in memory.
+ *
+ * @author Renan Gomes Barreto
+ * @version 1.0
+ * @since 17-06-2015
+ */
 package com.renangomes.alexarankcache;
 
 import java.io.BufferedInputStream;
@@ -17,42 +26,77 @@ import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
 
 /**
+ * AlexaRank class that will generate (on class load) a rank with top 1M URLs on
+ * Alexa.
+ *
+ * NOTE: This class will download and index the alexa top 1M rank once it is
+ * loaded. This means that it can add a few seconds delay on the software load
+ * time.
  *
  * @author Renan Gomes Barreto
+ * @version 1.0
+ * @since 17-06-2015
  */
 public class AlexaRank {
 
+    /**
+     * The singleton instace for the Alexa Rank Obj
+     */
     private static final AlexaRank instance = new AlexaRank();
 
+    /**
+     * A Linked Hash Map with the data indexed by Rank.
+     */
     private static Map<Integer, String> rankTable;
 
+    /**
+     * A Linked Hash Map with the data indexed by Domain.
+     */
     private static Map<String, Integer> domainTable;
 
+    /**
+     *
+     */
     private AlexaRank() {
         rankTable = Collections.synchronizedMap(new LinkedHashMap<Integer, String>(100));
         domainTable = Collections.synchronizedMap(new LinkedHashMap<String, Integer>(100));
         try {
             downloadAlexaTop1M();
         } catch (IOException ex) {
-            Logger.getLogger(AlexaRank.class.getName()).log(Level.SEVERE, null, ex);
+            Logger.getLogger(AlexaRank.class.getName()).log(Level.SEVERE, "Error Reading the Alexa Rank CSV file. All Requests to this class will return null.", ex);
         }
     }
 
-    // Runtime initialization
-    // By defualt ThreadSafe
+    /**
+     * Get A singleton object from this class
+     *
+     * @return A object of the class AlexaRank
+     */
     public static AlexaRank getInstance() {
         return instance;
     }
 
-    private LinkedHashMap<Integer, String> downloadAlexaTop1M() throws IOException, MalformedURLException {
+    /**
+     * Download, unzip and index the CSV files inside the alexa Rank top 1M. It
+     * keeps a cache on the folder "cache". The cache will be saved for 15 days
+     * before re-download.
+     *
+     * @throws IOException When the zip files can not be read or can not be
+     * saved/indexed.
+     * @throws MalformedURLException If the Alexa Top 1M zip file have an
+     * invalid URL
+     */
+    private void downloadAlexaTop1M() throws IOException, MalformedURLException {
         rankTable.clear();
         domainTable.clear();
 
         File cacheFolder = new File("cache");
         cacheFolder.mkdirs();
+        
         File zipFile = new File(cacheFolder, "top-1m.csv.zip");
         URL downloadURL = new URL("http://s3.amazonaws.com/alexa-static/top-1m.csv.zip");
         int cacheDays = 15;
+        
         if (!zipFile.exists() || !zipFile.canRead() || zipFile.lastModified() < (System.currentTimeMillis() - cacheDays * 24 * 60 * 60 * 1000)) {
             saveUrl(downloadURL, zipFile);
         }
@@ -91,10 +135,17 @@ public class AlexaRank {
             zipFile.delete();
             throw ioe;
         }
-        return null;
     }
 
-    private void saveUrl(final URL url, final File outputFile) throws MalformedURLException, IOException {
+    /**
+     * Download a URL and saves into a file
+     *
+     * @param url The source URL
+     * @param outputFile The output file
+     * @throws IOException If is not possible to download the file or write the
+     * output file. If an Exception is thrown, the outputFile is deleted.
+     */
+    private void saveUrl(final URL url, final File outputFile) throws IOException {
         if (url == null || outputFile == null) {
             throw new IllegalArgumentException("Illegal Argument. Any of the arguments can NOT be null.");
         }
@@ -122,29 +173,56 @@ public class AlexaRank {
         }
     }
 
+    /**
+     * Read the Rank for the domain name provided. NOTE: Before the search is
+     * done, this function will try to clear the URL in order to extract only a
+     * valid domain name.
+     *
+     * @param domain The string with a domain name or a partial URL
+     * @return The Rank position. NULL if the domain is not found.
+     */
     public Integer getRank(String domain) {
 
         if (domain == null || domain.trim().isEmpty()) {
             return null;
         }
-        return domainTable.get(domain);
+        //Simple Cleanup
+        String[] sp = domain.trim().toLowerCase().replaceAll("www.", "").replaceAll("http://", "").replaceAll("https://", "").split("/", 2);
+        return domainTable.get(sp[0]);
     }
 
+    /**
+     * Read the Domain for the rank provided.
+     *
+     * @param rank The integer with the position
+     * @return The domain that is on the position provided. NULL if the position
+     * is not available.
+     */
     public String getDomain(Integer rank) {
         return rankTable.get(rank);
     }
 
+    /**
+     * This is a sample usage of this class. It try to check some Domains and
+     * few rank positions.
+     *
+     *
+     * @param args Comand line arguments
+     */
     public static void main(String[] args) {
 
-        String[] domainList = {"google.com", "georanker.com", "www.georanker.com", "www.georanker.com/plans",
-            "https://www.georanker.com/", "alexa.com", "doesnotexists123456.com.au",};
-
+        //Check some domains names and some partial URLs.
+        String[] domainList = {"google.com", "georanker.com", "HTTP://georanker.com", "HTTP://WWW.GeoRanker.com/contactus", "www.georanker.com",
+            "www.georanker.com/plans", "https://www.georanker.com/", "alexa.com", "doesnotexists123456.com.au", "", "    ", null};
+        System.out.println("Get Rank by URL:");
         for (String domain : domainList) {
-            System.out.println("Alexa Rank for domain '" + domain + "': " + AlexaRank.getInstance().getRank(domain));
+            System.out.println(" - Alexa Rank for domain '" + domain + "': " + AlexaRank.getInstance().getRank(domain));
         }
 
-        for (int rank = 1; rank < 100; rank++) { 
-            System.out.println(rank + " -> " + AlexaRank.getInstance().getDomain(rank));
+        //Output the top 20 domains
+        System.out.println("Get Domain by Rank:");
+        for (int rank = 1; rank < 20; rank++) {
+            System.out.println(" - " + rank + " : " + AlexaRank.getInstance().getDomain(rank));
         }
     }
 }
